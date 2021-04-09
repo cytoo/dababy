da93f38946781dfcfd8a09e5e216d6c85753beea
cytolytic <71159394+cytoo@users.noreply.github.com>
2021-04-09 20:59:48 +0200

    main.rs: sus

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod data;
 
 extern crate structopt;
 
-use crate::data::dababy::{DABABY, URL};
+use crate::data::dababy::{DABABY, SUS_URL, LETS_GO_URL, SUS};
 use std::{
     thread::sleep,
     time::Duration,
@@ -16,6 +16,8 @@ use structopt::StructOpt;
 use colored::Colorize;
 use reqwest::Error;
 use rodio::{OutputStream, Decoder};
+use rand::{thread_rng, Rng};
+use std::borrow::Borrow;
 
 #[derive(StructOpt)]
 struct Opts {
@@ -35,30 +37,52 @@ fn main() {
         &message.push_str(x.as_str());
         &message.push_str(" ");
     });
-    print_dababy(message.clone(), opts.blue);
-    run_audio(&message, URL);
+    if message.contains("sus") {
+        print_dababy(message.clone(), opts.blue, true);
+        run_audio(&message, SUS_URL);
+        return;
+    }
+    print_dababy(message.clone(), opts.blue, false);
+    run_audio(&message, LETS_GO_URL);
 }
 
-fn print_dababy(message: String, blue: bool) {
+fn print_dababy(message: String, blue: bool, sus:bool) {
     println!("\n{}{}", " ".repeat(20), "-".repeat(message.chars().count() + 4));
     println!("{}| {} |", " ".repeat(20), message);
     println!("{}{}", " ".repeat(20), "-".repeat(message.chars().count() + 4));
     println!(r"{}\ /", " ".repeat(21 + message.chars().count() / 2));
     println!(r"{}\ /", " ".repeat(21 + message.chars().count() / 2));
-    if blue { println!("{}", DABABY.blue()); } else { println!("{}", DABABY); }
+    if sus {
+        println!("{}", SUS.red());
+    }
+    else if blue { println!("{}", DABABY.blue()); } else { println!("{}", DABABY); }
 }
 
 fn run_audio(message: &String, url: &'static str){
-    if message.to_lowercase().contains("go") {
+    let get_audio = |sus: bool| {
         let resp = match reqwest::blocking::get(url) {
             Ok(res) => res,
             Err(x) => {return ();}
         };
         let mut cursor = Cursor::new(resp.bytes().unwrap());
-        println!("{}", "'GO' SPOTTED IN THE TEXT".red());
         let (_strm, strm_hnd) = OutputStream::try_default().unwrap();
         let src = Decoder::new(cursor).unwrap();
         strm_hnd.play_raw(src.convert_samples()).unwrap();
-        sleep(Duration::from_secs(4));
+        let mut timer = 0;
+        if sus {
+            loop {
+                println!("{}", "DU ".repeat(thread_rng().gen_range(1..20)).as_str().red());
+                sleep(Duration::from_secs(1));
+                if timer >= 72 {break;}
+                timer += 1;
+            }
+        }
+    };
+    if message.to_lowercase().contains("go") {
+        println!("{}", "'GO' SPOTTED IN THE TEXT".red());
+        get_audio(false);
+    } else if message.contains("sus") {
+        println!("{}", "IMPOSTOR SUS".red());
+        get_audio(true);
     }
 }
